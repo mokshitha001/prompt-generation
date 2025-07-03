84b6f0ab727c33e8ea64adb8647e96206c326ae6
RAJESH <gurajalarajesh10@gmail.com>
2025-07-03 15:59:47 +0530

    replaced sensitive values in db url to env variables

--- prompt_gen/database/db_connect.py
+++ prompt_gen/database/db_connect.py
@@ -1,8 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://raj:1234@localhost/fastapidb"
+load_dotenv()
+
+
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
+port = os.getenv('DB_PORT', 5432)
+
+
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
